53acbb1a8894b7585f4c114f405ccabe6068b4e2
gopika <gopikalakshmia@gmail.com>
2024-12-30 17:27:04 -0500

    adaed payload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,22 +2,29 @@ import classes from './Counter.module.css';
 import { useSelector,useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const counter=useSelector(state=>state.count)
+  const counter=useSelector(state=>state.count);
+  const showCounter=useSelector(state=>state.showCounter);
   const dispatch=useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type:'Toggle'});
+  };
   const handleInc=()=>{
     dispatch({type:'Increment'})
   }
   const handleDec=()=>{
     dispatch({type:'Decrement'})
   }
+  const handleIncby=()=>{
+    dispatch({type:'Incby5',amount:5})
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter&&<div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={handleInc}>Increment</button>
+        <button onClick={handleIncby}>Increment by 5</button>
         <button onClick={handleDec}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import {createStore} from 'redux';
 
-const countReducer=(state={count:0},action)=>{
+const initialState={count:0,showCounter:true};
+const countReducer=(state=initialState,action)=>{
 if(action.type==='Increment'){
-    return {count:state.count+1}
+    return {count:state.count+1,showCounter:state.showCounter}
 }
 else if(action.type==='Decrement'){
-    return {count:state.count-1}
+    return {count:state.count-1,showCounter:state.showCounter}
+}
+else if(action.type==='Toggle'){
+    return {count:state.count,showCounter:!state.showCounter}
+}
+else if(action.type==='Incby5'){
+    return {count:state.count+action.amount,showCounter:state.showCounter}
 }
 return state;
 }
